4c79d6f80a72dbfe2b8e955b9ed52e0291997e2e
Renan Veronez Drechsler <renanvz@outlook.com>
2022-09-14 16:38:09 +0100

    Web - Box Add Text

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -60,6 +60,20 @@ function App() {
           </div>
         </a>
       </div>
+
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Nao encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anuncio para encontrar novos players!</span>
+          </div>
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded'>
+            Publicar anuncio
+          </button>
+        </div>
+
+      </div>
+
     </div>
   )
 }
