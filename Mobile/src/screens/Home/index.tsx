55c902a821addaa8d5e40ce6783fe16a8071409f
Renan Veronez Drechsler <renanvz@outlook.com>
2022-09-14 18:42:59 +0100

    Mobile: Flatlist

--- Mobile/src/screens/Home/index.tsx
+++ Mobile/src/screens/Home/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { GameCard } from '../../components/GameCard';
@@ -21,9 +20,20 @@ export function Home() {
         subtitle="Selecione o game que deseja jogar..."
       />
 
-      <GameCard
-        data={GAMES[0]}
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
       />
+
+
     </View>
   );
 }
