beba4fba4905263280f271b95881500aa3ec5795
Renan Veronez Drechsler <renanvz@outlook.com>
2022-09-14 15:50:52 +0100

    Tailwind

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,5 +1,17 @@
+import './styles/main.css';
+
+import logoImg from './assets/Logo.svg'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+
+      <h1 className="text-6xl text-white font-black">Seu
+        <span className="text-transparent bg-nlw-gradient bg-clip-text"> duo </span>
+        esta aqui.</h1>
+    </div>
+  )
 }
 
 export default App
