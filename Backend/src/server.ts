8d8aa12531b100bc14ed35601c430cfd84264c17
Renan Veronez Drechsler <renanvz@outlook.com>
2022-09-16 21:07:06 +0100

    Server - Routes

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,27 +1,72 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
-app.get('/games', (request, response) => {
-  return response.json([])
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        }
+      }
+    }
+  })
+
+  return response.json(games)
 })
 app.post('/ads', (request, response) => {
   return response.status(201).json([])
 })
 
 // www.minhaapi.com/ads
-app.get('/games/:id/ads', (request, response) => {
-  // const gameId = request.params.id;
-  return response.json([
-    { id: 1, name: 'Anuncio 1' },
-    { id: 2, name: 'Anuncio 2' },
-    { id: 3, name: 'Anuncio 3' },
-  ])
+app.get('/games/:id/ads', async (request, response) => {
+  const gameId = request.params.id;
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+      createdAt: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(',')
+    }
+  }))
 })
 
-app.get('/games/:id/discord', (request, response) => {
-  // const adId = request.params.id;
-  return response.json([])
+app.get('/ads/:id/discord', async (request, response) => {
+  const adId = request.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    }
+  })
+
+  return response.json({
+    discord: ad.discord,
+  })
 })
 
 app.listen(3333)
