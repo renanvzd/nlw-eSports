777a237c49b63389adda6cc703aa7a56a2260c5f
Renan Veronez Drechsler <renanvz@outlook.com>
2022-09-16 18:15:46 +0100

    HTTP Methods

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,13 +2,26 @@ import express from 'express';
 
 const app = express()
 
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+})
+
 // www.minhaapi.com/ads
-app.get('/ads', (request, response) => {
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
   return response.json([
     { id: 1, name: 'Anuncio 1' },
     { id: 2, name: 'Anuncio 2' },
     { id: 3, name: 'Anuncio 3' },
   ])
+})
+
+app.get('/games/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+  return response.json([])
 })
 
 app.listen(3333)
