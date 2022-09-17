3c8ffe9819ea40b0091b6b3521ddc492360981a8
Renan Veronez Drechsler <renanvz@outlook.com>
2022-09-17 16:53:27 +0100

    Consumir API Backend

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -3,8 +3,29 @@ import './styles/main.css';
 import logoImg from './assets/Logo.svg'
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
+
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" className='w-80' />
@@ -14,12 +35,16 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
-        <GameBanner bannerUrl="/game-1.png" title="League of Legends" adsCount={5} />
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
 
       <CreateAdBanner />
